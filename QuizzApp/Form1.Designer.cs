2fa0f6e4f3d16a44ff005354f4b7f3dee54093ea
RobertBlackwell01 <91064335+RobertBlackwell01@users.noreply.github.com>
2022-10-25 23:04:51 +0100

    UI updated

--- QuizzApp/Form1.Designer.cs
+++ QuizzApp/Form1.Designer.cs
@@ -35,31 +35,36 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(60, 36);
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(133, 424);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(103, 54);
+            this.button1.Size = new System.Drawing.Size(121, 148);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Single Player";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(60, 96);
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(307, 424);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(103, 53);
+            this.button2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button2.Size = new System.Drawing.Size(132, 148);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Two Player";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(1024, 755);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
